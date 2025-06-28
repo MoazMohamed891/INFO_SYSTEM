73dab5bba58f3c1e28b3f318e2cdd48b852d40e0
Moaz_Mohamedx3 <moaazh099@gmail.com>
2025-06-28 01:21:19 -0400

    Add files via upload
    sudo pip install -r requirements.txt

---
+++ info-system.py
@@ -0,0 +1,48 @@
+import platform
+import sys
+import os
+import time
+##############################################################################################
+print ("\033[31m")
+
+os.system("figlet Hallo To Moaz Mohamed Script")
+
+time.sleep(3)
+
+os.system("clear")
+
+
+print ("\033[1;31m")
+
+print ("#"*67)
+
+print ("\033[1;34m")
+
+os.system('figlet INFO-SYSTEM ')
+
+print ("\033[1;31m")
+
+print ("\033[35m")
+print (" \033[93;5m⚡\033[0m \033[35mBY.Moaz Mohamed\033[93;5m ⚡\033[0m")
+print ("\033[36m")
+print ("Github : https://github.com/MoazMohamed891")
+print ("Linkedin : https://www.linkedin.com/in/moaaz-mohamed-hassan-07604a348")
+print ("\033[1;31m")
+
+print ("#"*67)
+##############################################################
+
+print ("\033[1;34m")
+print("System       :", platform.system())
+print("Node         :", platform.node())
+print("Release      :", platform.release())
+print("Version      :", platform.version())
+print("Machine      :", platform.machine())
+print("Processor    :", platform.processor())
+print("Architecture :", platform.architecture())
+print("Platform     :", platform.platform())
+print("Python Ver.  :", platform.python_version())
+print("Python Build :", platform.python_build())
+print("Python Comp. :", platform.python_compiler())
+
+sys.exit()
